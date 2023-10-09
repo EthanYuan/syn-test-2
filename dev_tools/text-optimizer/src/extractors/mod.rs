82dac8131f9a71e4ec54781fbda8fd26351d791c
EthanYuan <yuanchengxing@hotmail.com>
2023-10-09 23:37:11 +0800

    Complete implementation of StdOutputExtractor

--- dev_tools/text-optimizer/src/extractors/mod.rs
+++ dev_tools/text-optimizer/src/extractors/mod.rs
@@ -3,7 +3,7 @@ pub mod log_extractor;
 pub mod std_output_extractor;
 pub mod thiserror_extractor;
 
-use crate::{yaml_processor::save_yaml, CLAP_TEXT_FILE, LOG_TEXT_FILE};
+use crate::{yaml_processor::save_yaml, CLAP_TEXT_FILE, LOG_TEXT_FILE, STD_OUTPUT_TEXT_FILE};
 use clap_extractor::ClapExtractor;
 use log_extractor::LogExtractor;
 use std_output_extractor::StdOutputExtractor;
@@ -20,7 +20,7 @@ use std::{
 pub fn extract(project_root: PathBuf, output_dir: &PathBuf) {
     // extractors
     let mut log_extractor = LogExtractor::new();
-    let mut std_output_extractor = StdOutputExtractor;
+    let mut std_output_extractor = StdOutputExtractor::new();
     let mut thiserror_extractor = ThiserrorExtractor;
     let mut clap_extractor = ClapExtractor::new();
 
@@ -57,6 +57,11 @@ pub fn extract(project_root: PathBuf, output_dir: &PathBuf) {
         clap_extractor.get_text_list(),
     )
     .expect("save yaml");
+    save_yaml(
+        &output_dir.join(STD_OUTPUT_TEXT_FILE),
+        std_output_extractor.get_text_list(),
+    )
+    .expect("save yaml");
 }
 
 pub fn process_rs_files_in_src(
@@ -76,6 +81,7 @@ pub fn process_rs_files_in_src(
                     // reset file path
                     log_extractor.reset_analysis_path(&file_path);
                     clap_extractor.reset_analysis_path(&file_path);
+                    std_output_extractor.reset_analysis_path(&file_path);
 
                     let file_content = fs::read_to_string(&file_path).expect("Failed to read file");
                     if let Ok(syntax_tree) = syn::parse_file(&file_content) {
--- dev_tools/text-optimizer/src/extractors/std_output_extractor.rs
+++ dev_tools/text-optimizer/src/extractors/std_output_extractor.rs
@@ -1,7 +1,33 @@
 use super::extract_contents_in_brackets;
+use crate::types::{Category, Meta, TextInfo};
+use std::path::PathBuf;
+use std::str::FromStr;
+use syn::spanned::Spanned;
 use syn::Macro;
 
-pub struct StdOutputExtractor;
+#[derive(Default)]
+pub struct StdOutputExtractor {
+    list: Vec<TextInfo>,
+    file_path: PathBuf,
+}
+
+impl StdOutputExtractor {
+    pub fn new() -> Self {
+        StdOutputExtractor::default()
+    }
+
+    pub fn reset_analysis_path(&mut self, file_path: &PathBuf) {
+        self.file_path = file_path.to_owned();
+    }
+
+    pub fn add_text_info(&mut self, text_info: TextInfo) {
+        self.list.push(text_info)
+    }
+
+    pub fn get_text_list(&self) -> &[TextInfo] {
+        &self.list
+    }
+}
 
 impl syn::visit::Visit<'_> for StdOutputExtractor {
     fn visit_macro(&mut self, node: &Macro) {
@@ -10,8 +36,15 @@ impl syn::visit::Visit<'_> for StdOutputExtractor {
             if ident == "println" || ident == "eprintln" {
                 let lit = node.tokens.to_string();
 
-                if let Some(format_string) = extract_contents_in_brackets(lit.to_owned()) {
-                    println!("Found format string: {}", format_string);
+                if let Some(text) = extract_contents_in_brackets(lit.to_owned()) {
+                    println!("Found format string: {}", text);
+
+                    let span = node.span();
+                    let start_line = span.start().line;
+                    let end_line = span.end().line;
+                    let category = Category::from_str(ident.to_string().as_str()).unwrap();
+                    let meta = Meta::new(category, self.file_path.to_owned(), start_line, end_line);
+                    self.add_text_info(TextInfo::new(text, meta));
                 }
             }
         }
