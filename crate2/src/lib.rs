a7b1dc27a38f0b81b0f5388d36bdf814d3d1074d
EthanYuan <yuanchengxing@hotmail.com>
2023-10-10 16:25:20 +0800

    fix std output extractor: code line

--- crate2/src/lib.rs
+++ crate2/src/lib.rs
@@ -2,6 +2,15 @@ pub fn add(left: usize, right: usize) -> usize {
     let hello = "Hello, world!";
     println!("Ethan says: {}", hello);
     eprintln!("Customizing consensus parameters for chain spec only works for dev chains.");
+    eprintln!(
+        "For optimal performance, CKB wants to migrate the data into new format.\n\
+        You can use the old version CKB if you don't want to do the migration.\n\
+        We strongly recommended you to use the latest stable version of CKB, \
+        since the old versions may have unfixed vulnerabilities.\n\
+        Run `\"{}\" migrate -C \"{}\"` and confirm by typing \"YES\" to migrate the data.\n\
+        We strongly recommend that you backup the data directory before migration.",
+        "bin_name", "root_dir"
+    );
 
     left + right
 }
